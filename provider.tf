3b930fb66f24f5ebcb6d42e9bd2caa8f6f3f8b3d
AnilNarayanaswamym <58822835+AnilNarayanaswamym@users.noreply.github.com>
2021-06-28 09:58:56 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,9 +2,3 @@ provider "google" {
   project = var.project_id
   region = var.region
 }
-
-# Creates a GCS bucket to store tfstate.
-resource "google_storage_bucket" "tfstate" {
-  name     = var.bucket_name
-  location = var.region
-}
